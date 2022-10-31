4a6c713bb641258787e094ee859da1b2130efa14
BabyTide <vertina2234@mail.ru>
2022-10-31 19:42:07 +0300

    Initial commit

---
+++ app/src/main/java/com/example/lab01_illarionov_493_data_transfer/G.java
@@ -0,0 +1,5 @@
+package com.example.lab01_illarionov_493_data_transfer;
+
+public final class G { //493 Illarionov
+    static DataBase messages;
+}
---
+++ app/src/main/java/com/example/lab01_illarionov_493_data_transfer/SettingsActivity.java
@@ -0,0 +1,56 @@
+package com.example.lab01_illarionov_493_data_transfer;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SettingsActivity extends AppCompatActivity {
+    //493 Illarionov
+    EditText ip,send_port,recieve_port,name;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings);
+
+        ip = findViewById(R.id.et_ip);
+        send_port = findViewById(R.id.et_sendPort);
+        recieve_port = findViewById(R.id.et_recievePort);
+        name = findViewById(R.id.et_name);
+
+        Intent i = getIntent();
+        name.setText(i.getStringExtra("name"));
+        ip.setText(i.getStringExtra("ip"));
+        send_port.setText(i.getStringExtra("sendPort"));
+        recieve_port.setText(i.getStringExtra("recievePort"));
+
+    }
+
+    public void onButtonApply_Click(View v){
+
+        Intent i = new Intent();
+        i.putExtra("ip", ip.getText().toString());
+        i.putExtra("sendPort", send_port.getText().toString());
+        i.putExtra("recievePort",recieve_port.getText().toString());
+        i.putExtra("name",name.getText().toString());
+        Log.e("TEST","FINISHING SETTINGS ACTIVITY");
+        setResult(RESULT_OK,i);
+        finish();
+    }
+
+    public void onButtonClearHistory_Click(View v){
+
+        G.messages.CLEARALLTABLE();
+        Toast.makeText(this, "Successfully cleared history", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onButtonExit_Click(View v){
+
+        setResult(RESULT_CANCELED);
+        finish();
+    }
+}
